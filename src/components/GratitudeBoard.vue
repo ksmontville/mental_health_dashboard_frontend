4b6af7acd3d4f8692d9dedb94ae2d9fb7ab2ea8b
Kyle <ksmontville@gmail.com>
2022-09-05 12:34:20 -0400

    Reconfigure canvas to use Konva package.

--- src/components/GratitudeBoard.vue
+++ src/components/GratitudeBoard.vue
@@ -4,41 +4,43 @@
       <DashboardNavbar />
     </nav>
 
-    <div class="canvas-wrapper">
+    <div class="canvas-wrapper" id="canvasContainer">
       <div class="toolbar">
-        <div class="container-fluid">
           <div class="input-group mb-3">
-
             <input ref= "imgInput" accept="image/*" type="file" class="form-control" id="imgUpload" v-on:change="">
-            <label class="input-group-text" for="" @click="getImageURL">Upload</label>
-
-
+            <label class="input-group-text" for="" @click="">Upload</label>
+            <button @click="draw">Draw Dot</button>
           </div>
-        </div>
       </div>
 
-      <button @click="clearCanvas">Clear</button>
+      <v-stage ref="stage" :config="config">
+        <v-layer @click="draw" ref="layer">
+          <v-rect id="canvas" :config="{x:0, y:0, width:400, height: 600, fill: 'white'}"
+                  @mousemove="draw" @mousedown="toggleMouseIsDown" @mouseup="toggleMouseIsDown"
+                  @touchmove="draw" @touchstart="toggleTouchStart" @touchend="toggleTouchStart"/>
+        </v-layer>
+      </v-stage>
 
-      <canvas ref="board" width="400" height="600" @touchmove="touch" @touchstart="toggleTouchStart" @touchend="toggleTouchStart"
-      @mousemove="draw" @mousedown="toggleMouseIsDown" @mouseup="toggleMouseIsDown">
-        This is a canvas
-      </canvas>
     </div>
   </div>
 </template>
 
 <script>
 
-import {onMounted, onUpdated, reactive, ref} from "vue";
+import {ref} from "vue";
+import Konva from "konva";
 
 export default {
   name: "GratitudeBoard",
 
   setup() {
-    const canvas = ref(null)
-    const image = ref('')
-    const imgInput = ref('')
-    const canvasImg = ref('')
+    const stage = ref()
+    const layer= ref()
+    const config = {
+      width: 400,
+      height: 600,
+    }
+
 
     const mouseX = ref()
     const mouseY = ref()
@@ -47,6 +49,39 @@ export default {
     const touchX = ref()
     const touchY = ref()
     const touchStart = ref(false)
+
+    function draw() {
+      const mousePos = stage.value.getNode().getPointerPosition()
+      const ctx = layer.value.getNode()
+
+      const dot = new Konva.Circle({
+        x: mousePos.x,
+        y: mousePos.y,
+        radius: 5,
+        fill: 'black',
+      })
+
+      if(mouseIsDown.value) {
+        ctx.add(dot)
+      }
+    }
+
+    function touch() {
+      const touchPos = stage.value.getNode().getPointerPosition()
+      const ctx = layer.value.getNode()
+
+      const dot = new Konva.Circle({
+        x: touchPos.x,
+        y: touchPos.y,
+        radius: 5,
+        fill: 'black'
+      })
+      
+      ctx.preventDefault()
+      if(touchStart.value) {
+        ctx.add(dot)
+      }
+    }
 
 
     function toggleMouseIsDown(event) {
@@ -57,69 +92,47 @@ export default {
       TouchEvent.type === 'touchstart' ? touchStart.value = true : touchStart.value = false
     }
 
-    function clearCanvas() {
-      const ctx = canvas.value.getContext('2d')
-      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-      imgInput.value.text = ''
-    }
+    // function draw(event) {
+    //   const ctx = canvas.value.getContext('2d')
+    //   let mx = event.offsetX
+    //   let my = event.offsetY
+    //
+    //   if (mouseIsDown.value) {
+    //     drawDot(mx, my, ctx)
+    //   }
+    // }
+    //
+    // function touch(TouchEvent) {
+    //   TouchEvent.preventDefault()
+    //   TouchEvent.stopPropagation()
+    //
+    //   const ctx = canvas.value.getContext('2d')
+    //   const canvasPos = canvas.value.getBoundingClientRect()
+    //   const touch = TouchEvent.touches[0]
+    //   const tx = touch.clientX - canvasPos.x
+    //   const ty = touch.clientY - canvasPos.y
+    //
+    //   if (touchStart.value) {
+    //     drawDot(tx, ty, ctx)
+    //   }
+    // }
+   // function getImageURL() {
+   //  const file = imgInput.value.files[0]
+   //
+   //  const reader = new FileReader()
+   //  reader.readAsDataURL(file)
+   //    reader.onload = () => {
+   //      drawImg(reader.result)
+   //    }
+   // }
 
-   function getImageURL() {
-    const file = imgInput.value.files[0]
 
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-      reader.onload = () => {
-        drawImg(reader.result)
-      }
-   }
-
-    function drawImg(path) {
-      const ctx = canvas.value.getContext('2d')
-      const img = new Image()
-
-      img.src = path
-      img.onload = () => {
-        // ctx.translate(200, 200)
-        ctx.drawImage(img, 50, 50, 100, 100)
-      }
-    }
-
-    function drawDot(x, y, ctx) {
-      ctx.beginPath()
-      ctx.arc(x, y, Math.PI, 0, 360)
-      ctx.fill()
-    }
-
-    function draw(event) {
-      const ctx = canvas.value.getContext('2d')
-      let mx = event.offsetX
-      let my = event.offsetY
-
-      if (mouseIsDown.value) {
-        drawDot(mx, my, ctx)
-      }
-    }
-
-    function touch(TouchEvent) {
-      TouchEvent.preventDefault()
-      TouchEvent.stopPropagation()
-
-      const ctx = canvas.value.getContext('2d')
-      const canvasPos = canvas.value.getBoundingClientRect()
-      const touch = TouchEvent.touches[0]
-      const tx = touch.clientX - canvasPos.x
-      const ty = touch.clientY - canvasPos.y
-
-      if (touchStart.value) {
-        drawDot(tx, ty, ctx)
-      }
-    }
 
       return {
-      board: canvas, mouseIsDown, mouseX, mouseY, touchStart, touchX, touchY, image, imgInput, canvasImg,
-        draw, clearCanvas, toggleMouseIsDown, toggleTouchStart, touch, drawImg, getImageURL,
+      stage, layer, config, mouseIsDown, mouseX, mouseY, touchStart, touchX, touchY,
+      draw, toggleMouseIsDown, touch, toggleTouchStart
       }
-    }
+    },
   }
 
 

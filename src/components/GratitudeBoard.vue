c83507e641cd2d9ae4db5bf47a03a949fae4a490
Kyle <ksmontville@gmail.com>
2022-09-07 15:43:54 -0400

    Work on scaling and selecting images... not done.

--- src/components/GratitudeBoard.vue
+++ src/components/GratitudeBoard.vue
@@ -11,14 +11,17 @@
             <label class="input-group-text" for="" @click="drawImg">Upload</label>
           </div>
           <button class="btn btn-info" @click="clear">Clear</button>
+        <button class="btn btn-info" @click="edit">Edit</button>
       </div>
 
       <v-stage ref="stage" :config="stageConfig">
-        <v-layer @click="draw" ref="layer">
-          <v-rect id="canvas" :config="canvasConfig"
+        <v-layer ref="layer">
+          <v-rect ref="canvas" :config="canvasConfig" @click="click" @tap="click"
                   @mousemove="draw" @mousedown="toggleMouseIsDown" @mouseup="toggleMouseIsDown"
                   @touchmove="touch" @touchstart="toggleTouchStart" @touchend="toggleTouchStart"/>
-          <v-image id="uploadedImage" />
+<!--          <v-circle ref="circle" @click="click" @tap="click" :config="{x: 100, y: 100, radius: 50, fill: 'black', name: 'circle'}" />-->
+          <v-image @click="click" @tap="click" ref="imgUploaded" :config="standby === false ? {image: imgUploaded.value} : null"/>
+          <v-transformer ref="transformer"/>
         </v-layer>
       </v-stage>
 
@@ -37,19 +40,27 @@ export default {
   setup() {
     const stage = ref()
     const stageConfig = {
+      name: 'stage',
       width: 400,
       height: 600,
     }
 
     const layer= ref()
+    const canvas = ref()
     const canvasConfig = {
+      name: 'canvas',
       width: 400,
       height: 600,
       fill: 'lavender'
     }
 
-    const imgInput = ref('')
+    const imgInput = ref()
     const imgUploaded = ref()
+    const standby = ref(true)
+
+    const circle = ref()
+
+    const transformer = ref()
 
     const mouseX = ref()
     const mouseY = ref()
@@ -59,18 +70,36 @@ export default {
     const touchY = ref()
     const touchStart = ref(false)
 
+
+    function getStageNode() {
+      return stage.value.getNode()
+    }
+
+    function getLayerNode() {
+      return layer.value.getNode()
+    }
+
+    function getTransformerNode() {
+      return transformer.value.getNode()
+    }
+
+
     function clear() {
-      const ctx = layer.value.getNode()
-      const children = ctx.children
+      const children = getLayerNode().children
 
       for(const child of children.slice(1, children.length)) {
           child.remove()
       }
     }
 
+    function edit() {
+      const shape = getStageNode().find('.myImage')
+      getTransformerNode().nodes([shape[0]])
+      console.log(shape[0])
+    }
+
     function draw() {
-      const mousePos = stage.value.getNode().getPointerPosition()
-      const ctx = layer.value.getNode()
+      const mousePos = getStageNode().getPointerPosition()
 
       const dot = new Konva.Circle({
         x: mousePos.x,
@@ -80,13 +109,12 @@ export default {
       })
 
       if(mouseIsDown.value) {
-        ctx.add(dot)
+        getLayerNode().add(dot)
       }
     }
 
     function touch() {
-      const touchPos = stage.value.getNode().getPointerPosition()
-      const ctx = layer.value.getNode()
+      const touchPos = getStageNode().getPointerPosition()
 
       const dot = new Konva.Circle({
         x: touchPos.x,
@@ -95,30 +123,36 @@ export default {
         fill: 'black'
       })
 
-      ctx.preventDefault()
+      getLayerNode().preventDefault()
       if(touchStart.value) {
-        ctx.add(dot)
+        getLayerNode().add(dot)
       }
     }
 
    function drawImg() {
-    const ctx = layer.value.getNode()
-    const newImage = new window.Image()
     const file = imgInput.value.files[0]
     const reader = new FileReader()
+    const newImage = new window.Image()
 
     reader.readAsDataURL(file)
       reader.onload = () => {
         newImage.src = reader.result
+
         imgUploaded.value = new Konva.Image({
           image: newImage,
+          name: 'myImage',
+          x: 100,
+          y: 100,
           width: 100,
           height: 100,
           draggable: true,
+          listening: true,
         })
-        ctx.add(imgUploaded.value)
+        console.log(imgUploaded.value)
+        getLayerNode().add(imgUploaded.value)
+        getTransformerNode().nodes([imgUploaded.value])
        }
-
+        standby.value = false
    }
 
     function toggleMouseIsDown(event) {
@@ -129,9 +163,26 @@ export default {
       TouchEvent.type === 'touchstart' ? touchStart.value = true : touchStart.value = false
     }
 
+    function click(event) {
+      const shape = getStageNode().find('.myImage')
+      console.log(event.target.name())
+      if(event.target.name() === 'canvas') {
+        getTransformerNode().nodes([])
+      }
+      else if(event.target.name() === 'circle') {
+        getTransformerNode().nodes([circle.value.getNode()])
+      }
+      else if (event.target.name() === shape[0].name()) {
+        console.log(event.type)
+        getTransformerNode().nodes([shape[0]])
+      }
+
+    }
+
     return {
-    stage, layer, stageConfig, imgInput, mouseIsDown, mouseX, mouseY, touchStart, touchX, touchY, canvasConfig, imgUploaded,
-    draw, toggleMouseIsDown, touch, toggleTouchStart, clear, drawImg,
+    stage, layer, canvas, stageConfig, imgInput, standby, mouseIsDown, mouseX, mouseY, touchStart, touchX, touchY,
+    canvasConfig, imgUploaded, transformer, circle,
+    draw, toggleMouseIsDown, touch, toggleTouchStart, clear, drawImg, click, edit,
       }
     },
   }

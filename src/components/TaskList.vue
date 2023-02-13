e862f726f7264a05b63b1d69750a410db55c3c9a
Kyle <ksmontville@gmail.com>
2023-02-13 11:27:45 -0500

    Change markTaskComplete parameters.

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -99,7 +99,7 @@
                     <button class="btn btn-dark" v-if="!task.canEdit" @click="task.canEdit = !task.canEdit" id="editButton">Edit (Coming Soon)</button>
                     <button class="btn btn-light" v-else @click="editTask(task).then(toggleCanEdit)" id="submitButton">Submit</button>
                     <button class="btn btn-secondary" @click="deleteTask(task)">Remove</button><br>
-                    <input class="btn-check" type="checkbox" id="isComplete" v-model="task.completed" @click="markTaskComplete(task.id)">
+                    <input class="btn-check" type="checkbox" id="isComplete" v-model="task.completed" @click="markTaskComplete(task)">
                     <label class="btn btn-outline-info" for="isComplete">Done!</label>
                   </div>
                   <p class="card-text p-2 mt-2" style="color:black;"><label v-if="task.completed"><em>&check; {{ task.date_completed }}</em></label></p>
@@ -248,7 +248,7 @@ export default {
           headers: {
             Authorization: `Bearer ${token}`
           }
-        }); await setTaskList(); console.log(task.id)
+        }); await setTaskList(); console.log(`${API_URL}/api/tasks/${task.id}`); console.log(task.id)
       } catch(error){
         console.log(error)
       }
